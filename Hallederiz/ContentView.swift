a5fd3f76005dba9d16941c4fcab36c951604892b
zeynepozgetemel <zeynepozgetemel@gmail.com>
2026-08-25 12:35:48 +0300

    Initial Commit

---
+++ Hallederiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hallederiz
+//
+//  Created by Zeynep Temel on 25.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hallederiz/HallederizApp.swift
@@ -0,0 +1,17 @@
+//
+//  HallederizApp.swift
+//  Hallederiz
+//
+//  Created by Zeynep Temel on 25.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct HallederizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
